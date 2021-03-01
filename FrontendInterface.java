f647b8f08d4dda23be6ee28eb4300b5ae2978794
YuvenSundaramoorthy <iamyuven@gmail.com>
2021-02-28 22:10:58 -0600

    Added main.java, and basic frontend complete

--- FrontendInterface.java
+++ FrontendInterface.java
@@ -2,46 +2,74 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class FrontendInterface {
-  public static void main(String[] args) throws IOException {
+  
+  public void main(String[] args) throws IOException {
     Backend backend = new Backend(args);
+    try {
+    Runtime.getRuntime().exec("clear");
+    }catch (Exception e) {}
     System.out.println("Welcome to the Movie Database");
-    backend.addGenre("Drama");
-    backend.addAvgRating("6");
+    backend.addGenre("all");
+    for(int i=0; i<11;i++) {
+      backend.addAvgRating(String.valueOf(i));
+    }
+    System.out.println(backend.getNumberOfMovies());
     baseMode(backend);
   }
-
-  private static void selectMode() {
-
+  private static void genreMode(Backend backend) {
+    for(int i =0; i<backend.getAllGenres().size();i++) {
+      backend.removeGenre(backend.getAllGenres().get(i));
+    }
+ 
   }
 
-  private static void genreMode() {
-
-  }
-
-  private static void ratingMode() {
-
+  private static void ratingMode(Backend backend) {
+    for(int i =0; i<11;i++) {
+      backend.removeAvgRating(String.valueOf(i));
+    }
   }
 
   private static void baseMode(Backend backend) {
     Scanner keyboard = new Scanner(System.in);
-    String key = "";
-    while (!key.equalsIgnoreCase("x")) {
+    String key = "0";
+    System.out.println("Top 3 Movies: ");
+    try {
+      System.out.println(backend.getThreeMovies(Integer.parseInt(key)).get(0).getTitle());
+    } catch (Exception e) {
+      System.out.println("No genre or rating selected. Press g or r to proceed");
+    }
+    System.out.println(backend.getThreeMovies(Integer.parseInt(key)).get(1).getTitle());
+    System.out.println(backend.getThreeMovies(Integer.parseInt(key)).get(2).getTitle());
+    key = keyboard.nextLine();
+    try {
+      Runtime.getRuntime().exec("clear");
+    } catch (IOException e) {
+
+    }
+    while (!key.equalsIgnoreCase("g") || !key.equalsIgnoreCase("r")) {
+      if (key.equalsIgnoreCase("g")) {
+        genreMode(backend);
+        return;
+      } else if (key.equalsIgnoreCase("r")) {
+        ratingMode(backend);
+        return;
+      }
       System.out.println("Top 3 Movies: ");
       try {
-        System.out.println(backend.getThreeMovies(0).get(0).getTitle());
+        System.out.println(backend.getThreeMovies(Integer.parseInt(key)-1).get(0).getTitle());
       } catch (Exception e) {
         System.out.println("No genre or rating selected. Press g or r to proceed");
-        String val = keyboard.nextLine();
-        // if(val.equalsIgnoreCase("g"))
       }
-      System.out.println(backend.getThreeMovies(0).get(1).getTitle());
-      System.out.println(backend.getThreeMovies(0).get(2).getTitle());
+      try {
+        System.out.println(backend.getThreeMovies(Integer.parseInt(key)-1).get(1).getTitle());
+      } catch (Exception e) {}
+      try {
+        System.out.println(backend.getThreeMovies(Integer.parseInt(key)-1).get(2).getTitle());
+      } catch (Exception e) {}
       key = keyboard.nextLine();
-    }
-    try {
-      Runtime.getRuntime().exec("cls");
-    } catch (IOException e) {
-      
+      try {
+        Runtime.getRuntime().exec("clear");
+      } catch (IOException e) {}
     }
   }
 }
---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    FrontendInterface frontend = new FrontendInterface(); 
+    frontend.main(args);
+  }
+}
