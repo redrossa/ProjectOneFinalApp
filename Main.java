0896af3acfe3d11a72b00e92d2a84e18771f418d
Reno Raksi <adriano.raksi@gmail.com>
2021-02-28 22:48:47 -0600

    Rollback

--- Main.java
+++ Main.java
@@ -1,12 +1,8 @@
-import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    Backend backend = new Backend(new FileReader("movies.csv"));
-    backend.addGenre("Drama");
-    for (int i = 0; i <= 10; i++)
-      backend.addAvgRating(String.valueOf(i));
-    System.out.println("end");
+    FrontendInterface frontend = new FrontendInterface();
+    frontend.main(args);
   }
 }
