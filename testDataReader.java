19e993493d249e1535b05d1cbbe98290de9106d3
boydlil <60155369+boydlil@users.noreply.github.com>
2021-03-02 10:53:24 -0600

    Added header to class

--- testDataReader.java
+++ testDataReader.java
@@ -1,6 +1,14 @@
 import java.io.FileReader;
 import java.util.List;
 
+//--== CS400 File Header Information ==--
+//Name: Lilly Boyd
+//Email: laboyd2@wisc.edu
+//Team: JF blue
+//Role: Data Wrangler
+//TA: Xinyi 
+//Lecturer: Gary
+//Notes to Grader: <optional extra notes>
 public class testDataReader {
   static MovieDataReader readerToTest;
   
